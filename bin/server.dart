0b3d91403d85b78a13e840b0b739e12dfa6a6cb8
Robert Ferreira <61979077+RobertsFerreira@users.noreply.github.com>
2022-09-25 23:21:26 -0300

    refatoração do arquivo inicial

--- bin/server.dart
+++ bin/server.dart
@@ -1,25 +1,25 @@
-import 'dart:io';
-
 import 'package:shelf/shelf.dart';
-import 'package:shelf/shelf_io.dart';
-import 'package:shelf_router/shelf_router.dart';
+
+import 'api/login/login_api.dart';
+import 'api/user/user.dart';
+import 'infra/server/custom_server.dart';
 
 // Configure routes.
-final _router = Router()..get('/sobre', _rootHandler);
+// final _router = Router()..get('/sobre', _rootHandler);
 
-Response _rootHandler(Request req) {
-  return Response.ok('API VERSION - 0.0.1');
-}
+// Response _rootHandler(Request req) {
+//   return Response.ok('API VERSION - 0.0.1');
+// }
 
-void main(List<String> args) async {
-  // Use any available host or container IP (usually `0.0.0.0`).
-  final ip = InternetAddress.anyIPv4;
-
+Future<void> main() async {
   // Configure a pipeline that logs requests.
-  final handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
+  // final handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
 
-  // For running in containers, we respect the PORT environment variable.
-  final port = int.parse(Platform.environment['PORT'] ?? '8080');
-  final server = await serve(handler, ip, port);
-  print('Server listening on port ${server.port}');
+  var cascadeHandler =
+      Cascade().add((LoginApi().handler)).add(User().handler).handler;
+
+  var handler =
+      Pipeline().addMiddleware(logRequests()).addHandler(cascadeHandler);
+
+  await CustomServer().initialize(handler);
 }
