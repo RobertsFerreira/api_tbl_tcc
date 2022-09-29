5c9445b7c765498882c8303f2fae472d1dded656
Robert Ferreira <robertsilva-ferreira@hotmail.com>
2022-09-28 23:50:27 -0300

    Criação da rota sobre

---
+++ bin/apis/sobre/sobre.dart
@@ -0,0 +1,14 @@
+import 'package:shelf/shelf.dart';
+import 'package:shelf_router/shelf_router.dart';
+
+class Sobre {
+  Handler get handler {
+    Router router = Router();
+
+    router.get('/sobre', (Request req) {
+      return Response.ok('API VERSION - 0.0.1');
+    });
+
+    return router;
+  }
+}
