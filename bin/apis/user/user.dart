6220dfae3cc49aefffa517ee367e1c66f6dc93ee
Robert Ferreira <robertsilva-ferreira@hotmail.com>
2022-09-28 23:50:05 -0300

    alteração da pasta dos arquivos

---
+++ bin/apis/user/user.dart
@@ -0,0 +1,18 @@
+import 'package:shelf/shelf.dart';
+import 'package:shelf_router/shelf_router.dart';
+
+class User {
+  Handler get handler {
+    Router router = Router();
+
+    router.get('/user', (Request req) {
+      return Response.ok('Funcionando');
+    });
+
+    router.post('/user', (Request req) {
+      return Response.ok('user post');
+    });
+
+    return router;
+  }
+}
