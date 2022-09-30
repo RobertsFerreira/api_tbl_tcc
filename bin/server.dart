0d90c6fef7e62ee30e65d6719569b934456c2119
Robert Ferreira <61979077+RobertsFerreira@users.noreply.github.com>
2022-09-29 23:17:52 -0300

    adição dos novos middleware e handlers

--- bin/server.dart
+++ bin/server.dart
@@ -1,16 +1,34 @@
 import 'package:shelf/shelf.dart';
 
 import 'apis/login/login_api.dart';
-import 'apis/user/user.dart';
+import 'apis/sobre/sobre_api.dart';
+import 'apis/types_user/type_user.dart';
+import 'apis/user/user_api.dart';
+import 'infra/middleware/middleware_interceptor.dart';
 import 'infra/server/custom_server.dart';
+import 'services/client/dio_client.dart';
+import 'services/interfaces/clients/http_client.dart';
+import 'services/types_user/types_user_service.dart';
 import 'utils/custom_env.dart';
 
 Future<void> main() async {
-  var cascadeHandler =
-      Cascade().add((LoginApi().handler)).add(User().handler).handler;
+  CustomEnv.fromFile('.env-dev');
 
-  var handler =
-      Pipeline().addMiddleware(logRequests()).addHandler(cascadeHandler);
+  final HttpClient client = DioClient();
+
+  var cascadeHandler = Cascade()
+      .add((LoginApi().handler))
+      .add(UserApi().handler)
+      .add(SobreApi().handler)
+      .add(
+        TypesUserApi(TypesUserService(client)).handler,
+      )
+      .handler;
+
+  var handler = Pipeline()
+      .addMiddleware(logRequests())
+      .addMiddleware(MiddlewareInterceptor().middleware)
+      .addHandler(cascadeHandler);
 
   await CustomServer().initialize(
     handler: handler,
