ede90e5de95585b991d567f7334697e2c9e140a6
Robert Ferreira <61979077+RobertsFerreira@users.noreply.github.com>
2022-10-01 13:58:53 -0300

    adição de tratamento de erro no middleware

--- lib/infra/middleware/middleware_interceptor.dart
+++ lib/infra/middleware/middleware_interceptor.dart
@@ -1,8 +1,30 @@
+import 'dart:convert';
+
+import 'package:map_fields/map_fields.dart';
 import 'package:shelf/shelf.dart';
 
 class MiddlewareInterceptor {
   Middleware get middleware {
     return createMiddleware(
+      errorHandler: (e, s) {
+        if (e is MapFieldsError) {
+          return Response.internalServerError(
+            body: jsonEncode({
+              'erro': e.toString(),
+              'message': 'Erro no processamento dos campos do json',
+              'stacktrace': s.toString(),
+            }),
+          );
+        } else {
+          return Response.internalServerError(
+            body: jsonEncode({
+              'erro': e.toString(),
+              'message': 'Erro interno desconhecido no servidor',
+              'stacktrace': s.toString(),
+            }),
+          );
+        }
+      },
       responseHandler: (Response response) => response.change(
         headers: {'Content-Type': 'application/json'},
       ),
