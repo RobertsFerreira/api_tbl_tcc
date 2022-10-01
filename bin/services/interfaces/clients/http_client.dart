e46c21aa8559953563d6a56d62fe5a845c94e144
Robert Ferreira <61979077+RobertsFerreira@users.noreply.github.com>
2022-10-01 12:31:19 -0300

    movimentação de pastas dos seguintes arquivos

--- bin/services/interfaces/clients/http_client.dart
+++
@@ -1,6 +0,0 @@
-abstract class HttpClient {
-  Future<dynamic> get(String url, {Map<String, dynamic>? queryParameters});
-  Future<dynamic> post(String url, {dynamic body});
-  Future<dynamic> put(String url, {dynamic body});
-  Future<dynamic> delete(String url, {Map<String, dynamic>? queryParameters});
-}
