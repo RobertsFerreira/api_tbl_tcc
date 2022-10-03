7cf55af0899b49d34c4f7a9159bb1d58e159050b
Robert Ferreira <61979077+RobertsFerreira@users.noreply.github.com>
2022-10-03 19:55:08 -0300

    criação do model para usuários já registrados

--- lib/models/user/user_model.dart
+++ lib/models/user/user_model.dart
@@ -1,31 +1,40 @@
+import 'package:api_tbl_tcc/core/models/user/user_default.dart';
 import 'package:map_fields/map_fields.dart';
 
 import '../type_user/type_user_model.dart';
 
-class UserModel {
+class UserModel extends UserDefault {
   final String id;
-  final String idCompany;
-  final String name;
-  final String cpf;
-  final TypeUserModel typeUser;
 
   UserModel({
     required this.id,
-    required this.idCompany,
-    required this.name,
-    required this.cpf,
-    required this.typeUser,
+    required super.name,
+    required super.cpf,
+    required super.birthDate,
+    required super.idCompany,
+    required super.typeUser,
   });
-
-  factory UserModel.fromJson(Map<String, dynamic> json) {
+  factory UserModel.fromMap(Map<String, dynamic> json) {
     final map = MapFields.load(json);
-    final type = (json['types_user'] ?? {});
+    final type = (json['type_user'] ?? {});
     return UserModel(
       id: map.getString('id', ''),
       idCompany: map.getString('id_company', ''),
       name: map.getString('name', ''),
       cpf: map.getString('cpf', ''),
+      birthDate: map.getDateTime('birth_date', DateTime.now()),
       typeUser: TypeUserModel.fromMap(type),
     );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'id_company': idCompany,
+      'name': name,
+      'cpf': cpf,
+      'birth_date': birthDate,
+      'id_type_user': typeUser.id,
+    };
   }
 }
