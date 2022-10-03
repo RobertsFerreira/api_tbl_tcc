6ba51f48e82e1ffd3212e15d1a435a09a078d39a
Robert Ferreira <61979077+RobertsFerreira@users.noreply.github.com>
2022-10-03 19:56:15 -0300

    adição dos tratamentos de erros

--- lib/core/models/errors/generic_error/generic_error.dart
+++ lib/core/models/errors/generic_error/generic_error.dart
@@ -1,11 +1,13 @@
 abstract class GenericError {
   final String message;
-  final String method;
-  final int statusCode;
 
   GenericError({
     required this.message,
-    required this.method,
-    required this.statusCode,
+  });
+}
+
+class UnknownError extends GenericError {
+  UnknownError({
+    required super.message,
   });
 }
