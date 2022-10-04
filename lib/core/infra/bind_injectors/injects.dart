207c452cda7072107ad5ef83f1c386cd1e291eee
Robert Ferreira <61979077+RobertsFerreira@users.noreply.github.com>
2022-10-04 00:51:22 -0300

    criação do bind para o user service

--- lib/core/infra/bind_injectors/injects.dart
+++ lib/core/infra/bind_injectors/injects.dart
@@ -1,7 +1,9 @@
 import 'package:api_tbl_tcc/export/export_functions.dart';
+import 'package:api_tbl_tcc/services/user/user_service.dart';
 
 import '../../../models/type_user/type_user_model.dart';
 import '../../interfaces/generic_service/generic_service.dart';
+import '../../models/user/user_default.dart';
 import 'bind_injectors.dart';
 
 class Injects {
@@ -12,6 +14,7 @@ class Injects {
     i.bind<TypesUserApi>(() => TypesUserApi(i()));
     i.bind<LoginApi>(() => LoginApi());
     i.bind<SobreApi>(() => SobreApi());
+    i.bind<GenericService<UserDefault>>(() => UserService(i()));
     return i;
   }
 }
