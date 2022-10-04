2c00a59482d7d5c0a0866843905f7c28a4644ec1
Robert Ferreira <61979077+RobertsFerreira@users.noreply.github.com>
2022-10-04 00:50:11 -0300

    adição da função de busca por id da empresa

--- lib/core/interfaces/generic_service/generic_service.dart
+++ lib/core/interfaces/generic_service/generic_service.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 abstract class GenericService<T> {
   Future<List<T>> getAll();
+  Future<List<T>> getAllForCompany({required String idCompany});
   Future<T> get(int id);
   Future<T> add(T t);
   Future<T> update(T t);
