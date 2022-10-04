62499d4500eec586bb1c6c830567c0245708c856
Robert Ferreira <61979077+RobertsFerreira@users.noreply.github.com>
2022-10-04 00:48:38 -0300

    alteração no response de teste

---
+++ test/utils/responses/users/users_response.dart
@@ -0,0 +1,19 @@
+String get userForCompanyResponse => '''
+{
+  "data": {
+    "user": [
+      {
+        "id": "bab4ab31-d22c-4180-9b53-e06390bd652b",
+        "name": "robert",
+        "cpf": "1234567890",
+        "birth_date": "2000-12-25",
+        "id_company": "1566d92f-9119-44d5-830e-9c3f94eb657c",
+        "types_user": {
+          "id": "103925a9-9b8f-4a4f-a50a-1980d46d3d89",
+          "name": "aluno"
+        }
+      }
+    ]
+  }
+}
+''';
