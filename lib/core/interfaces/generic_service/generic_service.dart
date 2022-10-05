0b4c5b350a1bcb33aacf602a98ce578463034970
Robert Ferreira <61979077+RobertsFerreira@users.noreply.github.com>
2022-10-04 21:22:25 -0300

    alteração dos nomes das funções

--- lib/core/interfaces/generic_service/generic_service.dart
+++ lib/core/interfaces/generic_service/generic_service.dart
@@ -1,10 +1,9 @@
 import 'dart:async';
 
 abstract class GenericService<T> {
-  Future<List<T>> getAll();
-  Future<List<T>> getAllForCompany({required String idCompany});
-  Future<T> get(int id);
-  Future<T> insert(T t);
-  Future<T> update(T t);
-  Future<T> delete(int id);
+  Future<List<T>> get({String? idCompany});
+  Future<T> getById(int id);
+  Future<bool> insert(T t);
+  Future<bool> update(T t);
+  Future<bool> delete(int id);
 }
