5a7600477753a03b022032fc8bdb80edd3ad5fe3
Robert Ferreira <61979077+RobertsFerreira@users.noreply.github.com>
2022-10-12 11:07:26 -0300

    Adição da feature de busca por turma

--- lib/apis/group/group_api.dart
+++ lib/apis/group/group_api.dart
@@ -1,11 +1,13 @@
 import 'dart:convert';
 
+import 'package:api_tbl_tcc/services/group/group_service.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
 import '../../core/interfaces/api/api.dart';
 import '../../core/interfaces/generic_service/generic_service.dart';
 import '../../core/models/group/group_default.dart';
+import '../../models/group/group_model.dart';
 import '../../models/group/new_group_model.dart';
 
 class GroupApi extends Api {
@@ -16,6 +18,27 @@ class GroupApi extends Api {
   @override
   Handler getHandler({List<Middleware>? middlewares}) {
     Router router = Router();
+
+    router.get('/group/<idClass>', (Request req, String idClass) async {
+      final groups = await (_groupService as GroupService).get(
+        idClass: idClass,
+      );
+      if (groups.isEmpty) {
+        return Response(204);
+      } else {
+        final groupMap = groups
+            .map(
+              (group) => (group as GroupModel).toMap(),
+            )
+            .toList();
+        final response = jsonEncode(
+          {
+            'groups': groupMap,
+          },
+        );
+        return Response.ok(response);
+      }
+    });
 
     router.post('/group', (Request req) async {
       final body = await req.readAsString();
