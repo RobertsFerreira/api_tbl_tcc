c02c353d5f9b9dc67c58634bcce1c7a05e871cb1
Robert Ferreira <robertsilva-ferreira@hotmail.com>
2022-10-20 17:16:41 -0300

    troca conversão da data

--- lib/models/quiz/quiz_model.dart
+++ lib/models/quiz/quiz_model.dart
@@ -1,5 +1,6 @@
 import 'package:api_tbl_tcc/core/models/quiz/quiz_default_model.dart';
 import 'package:api_tbl_tcc/models/quiz/question/quiz_question_model.dart';
+import 'package:api_tbl_tcc/utils/hasura/helper_extensions.dart';
 import 'package:map_fields/map_fields.dart';
 
 import '../user/user_model.dart';
@@ -38,7 +39,7 @@ class QuizModel extends QuizDefaultModel {
       'id': id,
       'id_class': idClass,
       'id_user': teacher.id,
-      'date': date,
+      'date': date.toDateHasura(),
       'number_question': numberQuestion,
       'id_company': idCompany
     };
@@ -49,7 +50,7 @@ class QuizModel extends QuizDefaultModel {
       'id': id,
       'id_class': idClass,
       'id_user': teacher.toMap(),
-      'date': date,
+      'date': date.toDateHasura(),
       'number_question': numberQuestion,
       'id_company': idCompany,
       'questions': questions.map((e) => (e as QuestionModel).toMap()).toList()
